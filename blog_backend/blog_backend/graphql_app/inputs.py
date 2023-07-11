12cabe8adf348fa5cef82daf5f457b0ceca0b9d2
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2023-07-12 02:59:48 +0300

    Added a missing class to GraphQL inputs

--- blog_backend/blog_backend/graphql_app/inputs.py
+++ blog_backend/blog_backend/graphql_app/inputs.py
@@ -1,6 +1,11 @@
 import graphene
 
 
+class TagInput(graphene.InputObjectType):
+    name = graphene.String()
+    slug = graphene.String()
+
+
 class PostInput(graphene.InputObjectType):
     title = graphene.String()
     tags = graphene.List(TagInput)
