a7f4a2ec75cfcb2020bcc64b9dd935d2eb4d7bb1
MoustafaShaaban <moustafa_shaaban@zohomail.com>
2024-01-09 22:23:08 +0200

    Setting up a vue frontend project

---
+++ vue-frontend/src/main.js
@@ -0,0 +1,58 @@
+import './assets/main.css'
+
+import { createApp, h } from 'vue'
+import { createPinia } from 'pinia'
+import { Quasar, Notify, Dialog, Dark, Cookies } from 'quasar'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import VueApolloComponents from '@vue/apollo-components'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp({
+    render: () => h(App),
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    cache,
+    uri: 'http://localhost:8000/graphql/',
+    credentials: 'include',
+    headers: {
+        'X-CSRFToken': Cookies.get('csrftoken')
+    }
+})
+
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+})
+
+app.use(createPinia())
+app.use(router)
+
+app.use(Quasar, {
+    plugins: {
+        Notify,
+        Dialog,
+        Dark,
+        Cookies,
+    }
+})
+
+app.use(VueQueryPlugin)
+
+app.use(apolloProvider)
+app.use(VueApolloComponents)
+
+app.mount('#app')
---
+++ vue-frontend/vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
