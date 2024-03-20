86940e4aa7071de1f9358ec5db89c972a81c808b
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2024-03-20 03:20:16 +0200

    Added a widget to htmx_crud app post model form

--- django_blog_backend/django_blog_backend/htmx_crud/forms.py
+++ django_blog_backend/django_blog_backend/htmx_crud/forms.py
@@ -8,3 +8,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'tag',)
+
+        widgets = {
+            'tag': forms.CheckboxSelectMultiple()
+        }
